5bb06aa76c728270188d0f16603717ad91abd7b1
sunkyuj <sunkyuj12@gmail.com>
2023-08-02 01:45:03 +0900

    Fix: 테스트 환경변수 추가 및 사용
    - 테스트 환경변수 추가 및 사용

--- src/test/java/com/sunkyuj/douner/jwt/JwtProviderTest.java
+++ src/test/java/com/sunkyuj/douner/jwt/JwtProviderTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.Date;
 
