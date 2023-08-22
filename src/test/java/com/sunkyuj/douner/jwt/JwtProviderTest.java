956237d6b81a0cdc1162ebe66703ba3fa02fe237
sunkyuj <sunkyuj12@gmail.com>
2023-08-22 17:33:15 +0900

    Test: 사용자 회원가입/로그인 테스트 추가
    - User register 테스트 코드 추가
    - User login 테스트 코드 추가

--- src/test/java/com/sunkyuj/douner/jwt/JwtProviderTest.java
+++ src/test/java/com/sunkyuj/douner/jwt/JwtProviderTest.java
@@ -10,9 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 
-import java.util.Date;
 
 //import static com.sunkyuj.douner.SecurityConstants.TOKEN_HEADER_PREFIX;
 
---
+++ src/test/java/com/sunkyuj/douner/user/UserRegisterLoginTest.java
@@ -0,0 +1,88 @@
+package com.sunkyuj.douner.user;
+
+import com.sunkyuj.douner.security.JwtService;
+import com.sunkyuj.douner.security.LoginToken;
+import com.sunkyuj.douner.user.model.UserLoginRequest;
+import com.sunkyuj.douner.user.model.UserLoginResultToken;
+import com.sunkyuj.douner.user.model.UserRegisterRequest;
+import com.sunkyuj.douner.user.model.UserRegisterResponse;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import static org.junit.jupiter.api.Assertions.*;
+@RunWith(SpringRunner.class)
+@Transactional
+@SpringBootTest
+class UserRegisterLoginTest {
+    @Autowired
+    UserService userService;
+    @Autowired
+    JwtService jwtService;
+
+    @Autowired
+    PlatformTransactionManager transactionManager;
+
+    TransactionStatus status;
+
+    @BeforeEach
+    void beforeEach() {
+        // 트랜잭션 시작
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+
+    @AfterEach
+    void afterEach() {
+        // 트랜잭션 롤백
+        transactionManager.rollback(status);
+    }
+
+    @Test
+    void joinUser() {
+        System.out.println("## User Register Test 시작 ##");
+        System.out.println();
+
+        //given
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest(
+                "testUser1","password","test1@test.com",UserType.REQUESTER);
+        int numOfUsersBefore = userService.findUsers().size();
+
+        //when
+        UserRegisterResponse result = userService.register(userRegisterRequest);
+        int numOfUsersAfter = userService.findUsers().size();
+
+
+        //then
+        assertEquals("testUser1", result.getName());
+        assertEquals("test1@test.com", result.getEmail());
+        assertEquals(numOfUsersBefore+1, numOfUsersAfter);
+    }
+
+    @Test
+    void loginUser() {
+        //given
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest(
+                "testUser2","password","test2@test.com",UserType.REQUESTER);
+        UserRegisterResponse userRegisterResponse = userService.register(userRegisterRequest);
+
+        UserLoginRequest userLoginRequest = new UserLoginRequest(
+                "test2@test.com","password",UserType.REQUESTER);
+
+        LoginToken loginToken = jwtService.createToken(userRegisterResponse.getUserId());
+
+        //when
+        UserLoginResultToken result = userService.login(userLoginRequest);
+
+        //then
+        assertEquals("testUser2",result.getUsername());
+        assertEquals(loginToken.getAccessToken(), result.getAccessToken());
+    }
+}
