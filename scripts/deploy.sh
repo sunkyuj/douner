a53b90a85451176da855a708311ab9e3b46a3f0d
sunkyuj <sunkyuj12@gmail.com>
2023-08-22 18:20:32 +0900

    Fix: 자동배포 쉘 스크립트 수정
    - 현재 PID를 올바르게 갖도록 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ubuntu/app
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+CURRENT_PID=$(pgrep -fla java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -18,7 +18,7 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar) #| tail -n 1)
 
 echo "> JAR NAME: $JAR_NAME"
 
