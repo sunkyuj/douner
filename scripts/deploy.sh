99f1dd2d2c01c4dcdc1272dd7e0fff5c1a8fc24c
sunkyuj <sunkyuj12@gmail.com>
2023-08-23 14:28:55 +0900

    Fix: CodeDeploy에서 환경변수를 불러올 수 있도록 코드 작성
    - source /home/ubuntu/.bashrc 실행

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,12 +20,7 @@ echo "> 새 애플리케이션 배포"
 
 echo "> 환경 변수 불러오기"
 
-source ~/.bashrc
-export DB_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DB_PASSWORD --query Parameters[0].Value | sed 's/"//g')
-export DB_URL=$(aws ssm get-parameters --region ap-northeast-2 --names DB_URL --query Parameters[0].Value | sed 's/"//g')
-export DB_USERNAME=$(aws ssm get-parameters --region ap-northeast-2 --names DB_USERNAME --query Parameters[0].Value | sed 's/"//g')
-export JWT_SECRET_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names JWT_SECRET_KEY --query Parameters[0].Value | sed 's/"//g')
-export TOKEN_HEADER_PREFIX=$(aws ssm get-parameters --region ap-northeast-2 --names TOKEN_HEADER_PREFIX --query Parameters[0].Value | sed 's/"//g')
+source /home/ubuntu/.bashrc
 
 JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
 
