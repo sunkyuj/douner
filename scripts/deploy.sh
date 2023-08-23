3c2fb624ff486fe45b3a5ad3e5c9b2211f3ec695
sunkyuj <sunkyuj12@gmail.com>
2023-08-23 14:41:39 +0900

    Fix: 배포 쉘 스크립트에 대한 로그 추가
    - deploy.sh가 로그를 찍도록 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 REPOSITORY=/home/ubuntu/app
+DEPLOY_LOG=$REPOSITORY/deploy.log
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
@@ -16,22 +17,26 @@ else
   sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
+TIME_NOW=$(date +%c)
 
-echo "> 환경 변수 불러오기"
+echo "> 새 애플리케이션 배포" >> $DEPLOY_LOG
 
-source /home/ubuntu/.bashrc
+echo "> 환경 변수 불러오기" >> $DEPLOY_LOG
+source /home/ubuntu/.bashrc >> $DEPLOY_LOG
 
-JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1) >> $DEPLOY_LOG
 
 #JAR_NAME="/home/ubuntu/app/douner-0.0.1-SNAPSHOT.jar"
 
-echo "> JAR NAME: $JAR_NAME"
+echo "> JAR NAME: $JAR_NAME" >> $DEPLOY_LOG
 
-echo "> $JAR_NAME 에 실행권한 추가"
+echo "> $JAR_NAME 에 실행권한 추가" >> $DEPLOY_LOG
 
-chmod +x $JAR_NAME
+chmod +x $JAR_NAME >> $DEPLOY_LOG
 
-echo "> $JAR_NAME 실행"
+echo "> $JAR_NAME 실행" >> $DEPLOY_LOG
 
 nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
