e9da4bfdbf1e7ae0d0422b1fb137e61dcf2ddc13
sunkyuj <sunkyuj12@gmail.com>
2023-08-23 15:24:04 +0900

    Fix: 외부 property 파일을 불러오도록 변경
    - deploy.sh에 외부 config 설정 코드 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,11 +23,11 @@ echo "> 새 애플리케이션 배포" >> $DEPLOY_LOG
 
 echo "> 환경 변수 불러오기" >> $DEPLOY_LOG
 #source /home/ubuntu/.bashrc >> $DEPLOY_LOG
-export DB_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DB_PASSWORD --query Parameters[0].Value | sed 's/"//g')
-export DB_URL=$(aws ssm get-parameters --region ap-northeast-2 --names DB_URL --query Parameters[0].Value | sed 's/"//g')
-export DB_USERNAME=$(aws ssm get-parameters --region ap-northeast-2 --names DB_USERNAME --query Parameters[0].Value | sed 's/"//g')
-export JWT_SECRET_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names JWT_SECRET_KEY --query Parameters[0].Value | sed 's/"//g')
-export TOKEN_HEADER_PREFIX=$(aws ssm get-parameters --region ap-northeast-2 --names TOKEN_HEADER_PREFIX --query Parameters[0].Value | sed 's/"//g')
+#export DB_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DB_PASSWORD --query Parameters[0].Value | sed 's/"//g')
+#export DB_URL=$(aws ssm get-parameters --region ap-northeast-2 --names DB_URL --query Parameters[0].Value | sed 's/"//g')
+#export DB_USERNAME=$(aws ssm get-parameters --region ap-northeast-2 --names DB_USERNAME --query Parameters[0].Value | sed 's/"//g')
+#export JWT_SECRET_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names JWT_SECRET_KEY --query Parameters[0].Value | sed 's/"//g')
+#export TOKEN_HEADER_PREFIX=$(aws ssm get-parameters --region ap-northeast-2 --names TOKEN_HEADER_PREFIX --query Parameters[0].Value | sed 's/"//g')
 
 
 JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1) >> $DEPLOY_LOG
@@ -42,7 +42,7 @@ chmod +x $JAR_NAME >> $DEPLOY_LOG
 
 echo "> $JAR_NAME 실행" >> $DEPLOY_LOG
 
-nohup java -jar -Duser.timezone=Asia/Seoul -D $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Duser.timezone=Asia/Seoul -Dspring.config.location=$REPOSITORY/application-deploy.properties $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
 
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
